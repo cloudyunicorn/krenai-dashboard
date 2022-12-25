3a75500b83d1dfe3c776db9a7f2afbe249cdff16
Rajat Singh <s.rajat55@gmail.com>
2022-12-25 20:32:52 +0530

    UI Changes & responsive
    Signed-off-by: Rajat Singh <s.rajat55@gmail.com>

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -6,10 +6,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sidebar.component.scss']
 })
 export class SidebarComponent implements OnInit {
+  planCard: any;
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  openStats() {
+    this.planCard = document.getElementById('planCard');
+    this.planCard.style.cssText = `
+      display: block !important;
+    `;
   }
 
 }
